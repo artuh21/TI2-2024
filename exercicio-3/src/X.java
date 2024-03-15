d64767b1ecd0adc7454223c961d70e716fce1c4a
artuh21 <130799136+artuh21@users.noreply.github.com>
2024-03-14 23:00:45 -0300

    Add files via upload

---
+++ exercicio-3/src/X.java
@@ -0,0 +1,12 @@
+package default package;
+
+public class X {
+    private int id;
+    private String nome;
+
+
+    @Override
+    public String toString() {
+        return "X [id=" + id + ", nome=" + nome + "]";
+    }
+}
